3d975936dd6e40b07dbf85798f16c1bcfd58d4ef
vilcinskij <vilcinskij@gmail.com>
2022-02-06 01:35:50 +0200

    rezultatas rodomas jeigu ne 0

--- rezervuaras/src/app/rezervuaras/rezervuaras.component.ts
+++ rezervuaras/src/app/rezervuaras/rezervuaras.component.ts
@@ -10,7 +10,7 @@ export class RezervuarasComponent implements OnInit {
   talpa: number;
   greitis: number;
   laikas: number;
-  rezultatas:number;
+  rezultatas:number = 0;
 
   skaiciuoti () {
     this.rezultatas=(this.laikas*this.greitis/this.talpa) * 100
