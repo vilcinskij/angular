f4e5d982ee034895a3c1f72848fbde13f88b6082
vilcinskij <vilcinskij@gmail.com>
2022-02-08 00:03:30 +0200

    preke.model.ts

--- todolist/src/app/pirkiniai/preke.model.ts
+++ todolist/src/app/pirkiniai/preke.model.ts
@@ -1,4 +1,10 @@
 export class Preke{
-   public pavadinimas:string;  // publick ir taip by default, bet rašome kad butu daugiau gražių žodžių
+    public pavadinimas:string;  // publick ir taip by default, bet rašome kad butu daugiau gražių žodžių
     public kiekis:string;
+    
+    constructor(pavadinimas:string, kiekis:string){
+        this.pavadinimas=pavadinimas;
+        this.kiekis=kiekis
+    }
+
 }
