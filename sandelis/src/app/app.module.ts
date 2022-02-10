7c3b64f705a8e1491e22f85b66e57de73fffb7ef
vilcinskij <vilcinskij@gmail.com>
2022-02-10 23:31:55 +0200

    warehouse

--- sandelis/src/app/app.module.ts
+++ sandelis/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent
   ],
   imports: [
     BrowserModule
