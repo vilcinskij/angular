85cc31253036d75b88fa343217aff61dafa2abdd
vilcinskij <vilcinskij@gmail.com>
2022-02-11 00:04:31 +0200

    input

--- sandelis/src/app/warehouse/products/products.component.ts
+++ sandelis/src/app/warehouse/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  products;
+  @Input() products;
 
   constructor() { }
 
