99d23f2190ab1d7d2f655b99dd43469133201e21
vilcinskij <vilcinskij@gmail.com>
2022-02-10 23:43:28 +0200

    wrh-data ts with data

---
+++ sandelis/src/app/warehouse/warehouse.data.ts
@@ -0,0 +1,48 @@
+[
+    {
+    name:"Buitine technika",
+    products:[
+    {
+    name:"Šaldytuvas",
+    price:380,
+    count: 3,
+    reserved: 1
+    },
+    {
+    name:"Skalbimo mašina",
+    price:210,
+    count: 5,
+    reserved: 2
+    },
+    {
+    name:"Mikrobangė",
+    price:80,
+    count: 6,
+    reserved: 0
+    }
+    ]
+    },
+    {
+    name:"Kompiuterinė technika",
+    products:[
+    {
+    name:"Monitorius LG",
+    price:260,
+    count: 12,
+    reserved: 0
+    },
+    {
+    name:"USB klaviatūra",
+    price:60,
+    count: 2,
+    reserved: 1
+    },
+    {
+    name:"Pelė",
+    price:30,
+    count: 8,
+    reserved: 5
+    }
+    ]
+    },
+    ];
